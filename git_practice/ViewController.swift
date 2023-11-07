1db9d7636340dad3cbe21f647e2c193100fd445c
knightri <63557646+knightri@users.noreply.github.com>
2023-11-07 14:56:35 +0530

    Update ViewController.swift

--- git_practice/ViewController.swift
+++ git_practice/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello world!")
+           print("hello world!")
+           print("hello world!")
+           print("hello world!")
         // Do any additional setup after loading the view.
     }
 
